d432de5cab14bc1d46e93bd14123c914af4227d8
calidadsedevilla-sketch <calidad.sede.villa@gmail.com>
2025-10-29 08:06:58 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,14 @@
-const CACHE = 'ocr-ov-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+  clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  // modo online first
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
